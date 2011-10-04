7a9ebb4e919037b8b039918d04640a8bdd68eb8c
Mike Cravey <techie@craveytrain.com>
2011-10-03 23:38:04 -0500

    Fixes whitespace of prompt file

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,28 +3,25 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
-    then
-      echo "%{\e[38;5;118m%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "%{\e[38;5;161m%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
+	st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+	if [[ $st == "" ]] then
+		echo ""
+	else
+		if [[ $st == "nothing to commit (working directory clean)" ]] then
+			echo "%{\e[38;5;118m%}$(git_prompt_info)%{$reset_color%}"
+		else
+			echo "%{\e[38;5;161m%}$(git_prompt_info)%{$reset_color%}"
+		fi
+	fi
 }
 
 git_prompt_info () {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
-echo "(${ref#refs/heads/})"
+	ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+	echo "(${ref#refs/heads/})"
 }
 
 directory_name () {
-  echo "%{\e[38;5;81m%}${PWD/#$HOME/~}%{$reset_color%}"
+	echo "%{\e[38;5;81m%}${PWD/#$HOME/~}%{$reset_color%}"
 }
 
 username () {
@@ -38,6 +35,6 @@ hostname () {
 export PROMPT=$'$(username) at $(hostname) in $(directory_name) $(git_dirty)\n› '
 
 precmd() {
-  title "zsh" "%m" "%55<...<%~"
+	title "zsh" "%m" "%55<...<%~"
 }
 
