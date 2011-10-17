c78f710cf89af2e1cf5406908cb1c162a24fc7ad
Mike Cravey <techie@craveytrain.com>
2011-10-16 22:05:34 -0500

    Making globs better

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,6 +21,10 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
+# superglobs
+setopt extendedglob
+unsetopt caseglob
+
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
