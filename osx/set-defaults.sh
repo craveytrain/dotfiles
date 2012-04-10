9d3eddbeac5b866e215f722330c27897c3dc81a0
Mike Cravey <techie@craveytrain.com>
2012-04-10 08:16:24 -0500

    Some nice osx defaults
    courtesy of @holman

---
+++ osx/set-defaults.sh
@@ -0,0 +1,17 @@
+# Sets reasonable OS X defaults.
+#
+# Or, in other words, set shit how I like in OS X.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
