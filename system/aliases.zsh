42eff505d6ae5c5a3d11ba7e43ec415471c11695
Mike Cravey <techie@craveytrain.com>
2012-04-17 18:20:37 -0500

    Adds catn for easy line numberings

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,14 +4,19 @@ alias -g 3..='../../..'
 alias -g 4..='../../../..'
 
 # Utilities
+# ls with those helpful little trailing characters
 alias ls="ls -F"
+# disk usage of current folder
 alias d="du -h -d=1"
+# Disk free space (human readable numbers)
 alias df="df -h"
+# Show history
 alias h="history"
-alias localip="ipconfig getifaddr en1"
+# Check my ip
 alias ip="curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g'"
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+# Colorize grep
 alias grep='GREP_COLOR="1;37;45" LANG=C grep --color=auto'
-
+# Concatenate and print content of files (add line numbers)
+alias catn="cat -n"
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
