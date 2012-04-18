80b358da1dbfbf21a848fbb7c04768b6f95c7d3b
Mike Cravey <techie@craveytrain.com>
2012-04-17 19:59:41 -0500

    Adds OSX utils

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -15,3 +15,9 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
+
+# Finder: disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Panels: disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
