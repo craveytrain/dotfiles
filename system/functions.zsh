ae870d4f89e6a9232c0704c0fe1fee4cf4763f27
Mike Cravey <techie@craveytrain.com>
2012-04-18 16:56:30 -0500

    Adds dataurl function

--- system/functions.zsh
+++ system/functions.zsh
@@ -24,3 +24,8 @@ ips () {
     echo "$i $(ipconfig getifaddr ${i%:})"
   done
 }
+
+# Create a data URL from an image (works for other file types too, if you tweak the Content-Type afterwards)
+dataurl() {
+  echo "data:image/${1##*.};base64,$(openssl base64 -in "$1")" | tr -d '\n' | pbcopy
+}
