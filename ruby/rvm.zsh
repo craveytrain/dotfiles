71371b2cd9e7983519424b20651e68fa30669f2c
Mike Cravey <techie@craveytrain.com>
2012-04-18 23:17:03 -0500

    Moving over to rbenv

--- ruby/rvm.zsh
+++
@@ -1,7 +0,0 @@
-# This loads RVM into a shell session.
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] then
-	alias rubies='rvm list rubies'
-	alias gemsets='rvm gemset list'
-	export CC="/usr/local/bin/gcc-4.2"
-	. "$HOME/.rvm/scripts/rvm"
-fi
