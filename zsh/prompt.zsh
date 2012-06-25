0bc7d405b63b9dbe1794111f32b1f5994df2d6c2
Mike Cravey <techie@craveytrain.com>
2012-06-25 17:09:44 +0100

    Adds mercurial support to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,22 +1,32 @@
 # load version control helpers
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' branchformat '%b:%r'
+zstyle ':vcs_info:*' enable git hg
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' get-revision true
+zstyle ':vcs_info:hg:*' branchformat '%b%F{reset}[%r%F{reset}]'
+zstyle ':vcs_info:*' hgrevformat '%F{yellow}%12.12h'
 zstyle ':vcs_info:*' stagedstr '%F{green}+%F{reset}'
 zstyle ':vcs_info:*' unstagedstr '%F{red}*%F{reset}'
-zstyle ':vcs_info:*' check-for-changes true
 
 git_dirty() {
-	st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-	if [[ $st != "" ]] then
-		if [[ $st == "nothing to commit (working directory clean)" ]] then
-			zstyle ':vcs_info:*' formats ' on (%F{green}%b%F{reset})%c%u'
-			zstyle ':vcs_info:*' actionformats ' on (%F{green}%b|%a%F{reset})%c%u'
-		else
-			zstyle ':vcs_info:*' formats ' on (%F{red}%b%F{reset})%c%u'
-			zstyle ':vcs_info:*' actionformats ' on (%F{red}%b|%a%F{reset})%c%u'
-		fi
+	git_st=$(git status --porcelain 2> /dev/null | tail -n 1)
+	if [[ $git_st == "" ]] then
+		zstyle ':vcs_info:git:*' formats ' on %F{green}%b%F{reset}[%F{yellow}%7.7i%F{reset}]%c%u'
+		zstyle ':vcs_info:git:*' actionformats ' on %F{green}%b%F{reset}[%F{yellow}%7.7i%F{reset}]|%a%c%u'
+	else
+		zstyle ':vcs_info:git:*' formats ' on %F{red}%b%F{reset}[%F{yellow}%7.7i%F{reset}]%c%u'
+		zstyle ':vcs_info:git:*' actionformats ' on %F{red}%b%F{reset}[%F{yellow}%7.7i%F{reset}]|%a%F{reset}%c%u'
+	fi
+}
+hg_dirty() {
+	hg_st=$(hg status 2> /dev/null | tail -n 1)
+	if [[ $hg_st == "" ]] then
+		zstyle ':vcs_info:hg:*' formats ' on %F{green}%b%F{reset}%c%u'
+		zstyle ':vcs_info:hg:*' actionformats ' on %F{green}%b%F{reset}|%a%c%u'
+	else
+		zstyle ':vcs_info:hg:*' formats ' on %F{red}%b%F{reset}%c%u'
+		zstyle ':vcs_info:hg:*' actionformats ' on %F{red}%b%F{reset}%c%u'
 	fi
 }
 
@@ -38,7 +48,7 @@ todo(){
 		num=$(echo $(todo.sh ls | wc -l))
 		#compensate for the extra 2 lines of cruft
 		let todos=num-2
-		
+
 		if [ $todos -gt 0 ]; then
 			if [ $todos -eq 1 ]; then
 				label="todo "
@@ -64,6 +74,7 @@ export RPROMPT='$(todo)'
 precmd() {
 	title "zsh" "%m" "%55<...<%~"
 	git_dirty
+	hg_dirty
 	vcs_info
 	print -Pn "\e]2; %~/ \a"
 }
