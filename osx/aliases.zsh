7c700ee7464d6c95088c86ebbae45e1a8194fd04
Mike Cravey <techie@craveytrain.com>
2012-07-30 10:43:31 -0500

    No longer need hub

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -3,9 +3,6 @@ alias ff="open -a Firefox.app $1"
 alias chrome="open -a 'Google Chrome.app' $1"
 alias safari="open -a Safari.app $1"
 
-# GitHub
-alias git=hub
-
 # Flush DNS cache
 alias flushdns="dscacheutil -flushcache"
 
