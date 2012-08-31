4cb2806f7f9a37c22c632530bdffd912848bf519
Mike Cravey <techie@craveytrain.com>
2012-08-31 15:27:30 -0500

    Changing default http server port to 8080

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,5 +1,5 @@
 if [[ $OSTYPE == linux* ]]; then
-	alias http='python2 -m SimpleHTTPServer'
+	alias http='python2 -m SimpleHTTPServer 8080'
 else
-	alias http='python -m SimpleHTTPServer'
+	alias http='python -m SimpleHTTPServer 8080'
 fi
