9df452e92e087e4796545f23d625d26fc344ded2
Mike Cravey <techie@craveytrain.com>
2012-09-13 23:55:14 -0500

    Environment hotness

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,7 +9,7 @@ zstyle ':vcs_info:*' hgrevformat '%F{yellow}%12.12h'
 zstyle ':vcs_info:*' stagedstr '%F{green}+%F{reset}'
 zstyle ':vcs_info:*' unstagedstr '%F{red}*%F{reset}'
 
-git_dirty() {
+git_dirty () {
 	git_st=$(git status --porcelain 2> /dev/null | tail -n 1)
 	if [[ $git_st == "" ]] then
 		zstyle ':vcs_info:git:*' formats ' on %F{green}%b%F{reset}[%F{yellow}%7.7i%F{reset}]%c%u'
@@ -19,7 +19,8 @@ git_dirty() {
 		zstyle ':vcs_info:git:*' actionformats ' on %F{red}%b%F{reset}[%F{yellow}%7.7i%F{reset}|%F{red}%a%F{reset}]%c%u'
 	fi
 }
-hg_dirty() {
+
+hg_dirty () {
 	hg_st=$(hg status 2> /dev/null | tail -n 1)
 	if [[ $hg_st == "" ]] then
 		zstyle ':vcs_info:hg:*' formats ' on %F{green}%b%F{reset}%c%u'
@@ -44,10 +45,16 @@ hostname () {
 
 # Right prompt magic
 rprompt () {
+	RB_VERSION=$(rbenv version-name)
+
 	if [ "$RB_VERSION" = "system" ]; then
-		echo "$(todo)"
+		if [ -z "$VIRTUALENV" ]; then
+			export RPROMPT="$(todo)"
+		else
+			export RPROMPT="$VIRTUALENV"
+		fi
 	else
-		echo "rbenv: %F{yellow}$RB_VERSION%F{reset}"
+		export RPROMPT="$VIRTUALENV rbenv: %F{yellow}$RB_VERSION%F{reset}"
 	fi
 }
 
@@ -78,13 +85,11 @@ fi
 
 export PROMPT2=$'› '
 
-export RPROMPT='$(rprompt)'
-
 precmd() {
 	title "zsh" "%m" "%55<...<%~"
 	git_dirty
 	hg_dirty
 	vcs_info
+	rprompt
 	print -Pn "\e]2; %~/ \a"
 }
-
