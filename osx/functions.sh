c9fc1f0c5d800434fffac2bb5c8a7fde7a21128b
Mike Cravey <techie@craveytrain.com>
2013-01-25 17:02:38 -0600

    Removes silly rm to trashbin

--- osx/functions.sh
+++ osx/functions.sh
@@ -1,22 +1,5 @@
 # OS X Only
 
-# make rm move to trash instead of traditional delete
-rm () {
-  local file_path
-  for file_path in "$@"; do
-    # ignore any arguments
-    if [[ "$file_path" = -* ]]; then :
-    else
-      local dst=${file_path##*/}
-      # append the time if necessary
-      while [ -e ~/.Trash/"$dst" ]; do
-        dst="$dst "$(date +%H-%M-%S)
-      done
-      mv "$file_path" ~/.Trash/"$dst"
-    fi
-  done
-}
-
 # growl
 growl () {
 	echo -e $'\e]9;'${1}'\007';
