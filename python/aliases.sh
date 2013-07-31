6c1fe09b4ab926a5b978bdbc0233823bf6cc03e4
Mike Cravey <techie@craveytrain.com>
2013-07-31 13:08:06 -0500

    Changes http server to 'serve'

--- python/aliases.sh
+++ python/aliases.sh
@@ -1,5 +1,5 @@
 if [[ $OSTYPE == linux* ]]; then
-	alias http='python2 -m SimpleHTTPServer 8080'
+	alias serve='python2 -m SimpleHTTPServer 8080'
 else
-	alias http='python -m SimpleHTTPServer 8080'
+	alias serve='python -m SimpleHTTPServer 8080'
 fi
