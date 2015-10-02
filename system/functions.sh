c082f73023cfdbc021a253d3205d75a3da91e193
Mike Cravey <mike@craveytrain.com>
2015-10-02 16:11:18 +0100

    Purty git diff

--- system/functions.sh
+++ system/functions.sh
@@ -44,3 +44,22 @@ serve () {
 digg() {
 	dig +nocmd "$1" any +multiline +noall +answer
 }
+
+strip_diff_leading_symbols(){
+    color_code_regex=$'(\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K])'
+
+        # simplify the unified patch diff header
+        sed -E "s/^($color_code_regex)diff --git .*$//g" | \
+               sed -E "s/^($color_code_regex)index .*$/\
+\1$(rule)/g" | \
+               sed -E "s/^($color_code_regex)\+\+\+(.*)$/\1\+\+\+\5\\
+\1$(rule)/g" | \
+
+        # actually strips the leading symbols
+               sed -E "s/^($color_code_regex)[\+\-]/\1 /g"
+}
+
+## Print a horizontal rule
+rule () {
+        printf "%$(tput cols)s\n"|tr " " "─"
+}
