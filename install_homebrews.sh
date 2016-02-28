bcfeccd9a97272d94bdbaf92f10a2b9298a3c6a6
Mike Cravey <techie@craveytrain.com>
2016-02-28 22:59:01 +0000

    Now installs helpful repos
    Also breaks out installs to specific scripts so they can be run
    individually.

---
+++ install_homebrews.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+brews=(
+	ack
+	bash
+	boot2docker
+	coreutils
+	findutils
+	git
+	homebrew/dupes/grep
+	httpie
+	imagemagick
+	jq
+	libyaml
+	mercurial
+	mobile-shell
+	ngrep
+	node
+	nvm
+	phantomjs
+	rbenv
+	ruby-build
+	the_silver_searcher
+	wget
+	zsh
+	)
+
+    # Install if we don't have it
+    hash brew 2>/dev/null || {
+    	echo "Installing homebrew..."
+    	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    }
+
+    echo "Updating and upgrading homebrew recipes"
+    brew update
+    brew upgrade
+    brew tap homebrew/dupes
+
+    echo "Brewing binaries"
+    brew install "${brews[@]}"
+
+    echo "Making option utils available"
+    ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
+
+    echo "Cleaning up your mess"
+    brew cleanup
---
+++ install_npm_packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nodes=(
+	babel
+	bookmarklet
+	bower
+	browserify
+	csscomb
+	csslint
+	eslint
+	gulp
+	grunt-cli
+	js-beautify
+	jscs
+	jsdoc
+	jshint
+	sitespeed.io
+	svgo
+)
+
+echo "Installing the nodes"
+npm install -g "${nodes[@]}"
