d628ea996bfd73890bd63f77cebc85c2e7a6180a
Mike Cravey <techie@craveytrain.com>
2017-11-10 12:06:26 -0600

    Adds nvm autoloader and localized npm binaries

---
+++ zsh/nvm_use.zsh
@@ -0,0 +1,28 @@
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+    path=(
+      $(npm bin)
+      $path
+    )
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+    path=(
+      $(npm bin)
+      $path
+    )
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
