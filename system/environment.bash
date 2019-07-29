1ef1459959a45b8945eea73c4eb5b44b30a8a344
Mike Cravey <mcravey@expedia.com>
2019-07-29 16:45:46 -0500

    Dircolors cross platform

--- system/environment.bash
+++ system/environment.bash
@@ -4,6 +4,17 @@
 # Environment
 #
 
+# export nice, clean platform names
+case "$OSTYPE" in
+  solaris*) PLATFORM="SOLARIS" ;;
+  darwin*)  PLATFORM="OSX" ;;
+  linux*)   PLATFORM="LINUX" ;;
+  bsd*)     PLATFORM="BSD" ;;
+  msys*)    PLATFORM="WINDOWS" ;;
+  *)        PLATFORM="unknown: $OSTYPE" ;;
+esac
+export PLATFORM
+
 # use vim if possible, otherwise vi
 if hash vim 2>/dev/null; then
   export EDITOR=vim
@@ -18,11 +29,16 @@ fi
 
 export PAGER='less'
 
-# Define colors for BSD ls.
-export LSCOLORS='gxfxcxdxbxGxDxabagacad'
+# Define colors for BSD ls
+export LSCOLORS='exfxbxdxcxegedabagacad'
+export CLICOLOR=1
 
-# Define colors for the completion system.
-export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+# Define colors for GNU ls
+export LS_COLORS='di=34:ln=35:so=31:pi=33:ex=32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+
+# if dircolors or gdircolors, run them
+hash dircolors 2>/dev/null && eval "$(dircolors -b "$DOTFILES/system/dir_colors")"
+hash gdircolors 2>/dev/null && eval "$(gdircolors -b "$DOTFILES/system/dir_colors")"
 
 # Grep
 export GREP_COLOR='1;90;103'           # BSD.
