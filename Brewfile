3d7ef48185682f6cdc152e5d6c31b1d0f6702399
Mike Cravey <mike@craveytrain.com>
2023-11-01 14:57:10 -0500

    Adds exa and swaps ls for it

--- Brewfile
+++ Brewfile
@@ -1,8 +1,16 @@
 tap "homebrew/bundle"
 tap "jandedobbeleer/oh-my-posh"
+# Extendable version manager with support for Ruby, Node.js, Erlang & more
 brew "asdf"
+# Good-lookin' diffs with diff-highlight and more
 brew "diff-so-fancy"
+# Modern replacement for 'ls'
+brew "exa"
+# GitHub command-line tool
 brew "gh"
+# Port scanning utility for large networks
 brew "nmap"
+# Organize software neatly under a single directory tree (e.g. /usr/local)
 brew "stow"
+# Prompt theme engine for any shell
 brew "jandedobbeleer/oh-my-posh/oh-my-posh"
--- zsh/.zsh/aliases.sh
+++ zsh/.zsh/aliases.sh
@@ -21,26 +21,37 @@ alias e='${(z)VISUAL:-${(z)EDITOR}}'
 # shellcheck disable=SC2139
 alias mkdir="${aliases[mkdir]:-mkdir} -p"
 
-if [ $PLATFORM = 'LINUX' ]; then
-  alias ls='ls --color=auto -F'
-elif hash gls 2>/dev/null; then
-  # use GNU ls cause it supports 256 colors and symbols
-  alias ls='gls --color=auto -F'
+
+# if exa is installed, used it
+if whence exa >/dev/null; then
+	alias ls="exa -F"
+	alias l='ls -1a'		# Lists in one column, hidden files.
+	alias la='ll -a'		# Lists human readable sizes, hidden files.
+	alias lx='ll -s=ext'	# Lists sorted by extension (GNU only).
+	alias lk='ll -s=size'	# Lists sorted by size, largest last.
+	alias lt='ll -T'		# Lists sorted by date, most recent last.
 else
-  alias ls="${aliases[ls]:-ls} -F -G"
+	if [ $PLATFORM = 'LINUX' ]; then
+  		alias ls='ls --color=auto -F'
+	elif hash gls 2>/dev/null; then
+  		# use GNU ls cause it supports 256 colors and symbols
+  		alias ls='gls --color=auto -F'
+	else
+	  	alias ls="${aliases[ls]:-ls} -F -G"
+	fi
+	alias l='ls -1A'         # Lists in one column, hidden files.
+	alias la='ll -A'         # Lists human readable sizes, hidden files.
+	alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+	alias lk='ll -Sr'        # Lists sorted by size, largest last.
+	alias lt='ll -tr'        # Lists sorted by date, most recent last.
+	alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+	alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+# These work across exa and ls
+alias ll='ls -lh'			# Lists human readable sizes.
+alias lm='la | "$PAGER"'	# Lists human readable sizes, hidden files through pager.
+alias sl='ls'				# I often screw this up.
 
 # Resource Usage
 alias df='df -kh'
