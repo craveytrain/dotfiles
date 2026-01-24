5b1777b2d6f1209b8c5b1918a43064761e9981cf
Mike Cravey <mike@craveytrain.com>
2026-01-23 23:19:36 -0600

    Formats EZA colors to not bold user

---
+++ modules/shell/files/.config/fish/config.fish
@@ -0,0 +1,6 @@
+#
+# Shell utilities environment (from shell module)
+#
+
+# eza colors - remove bold from various elements
+set -gx EZA_COLORS 'uu=33:un=33'
---
+++ modules/shell/files/.zsh/environment.sh
@@ -0,0 +1,6 @@
+#
+# Shell utilities environment (from shell module)
+#
+
+# eza colors - remove bold from various elements
+export EZA_COLORS='uu=33:un=33'
